17b4be111ba72d509588a42360ae2c83b40d09f7
z3db0y <us3r0002@gmail.com>
2025-12-31 00:44:59 +0200

    merge with Greatness upstream - update GoBildaOdometry.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gobilda/GoBildaOdometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gobilda/GoBildaOdometry.java
@@ -3,22 +3,44 @@ package org.firstinspires.ftc.teamcode.gobilda;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.robotcore.external.navigation.UnnormalizedAngleUnit;
 
 import dev.zedboy.greatness.Odometry;
+import dev.zedboy.greatness.math.vec2;
+import dev.zedboy.greatness.math.vec3;
 
 public class GoBildaOdometry extends Odometry {
     GoBildaPinpointDriver driver;
+
+    @Override
+    public void setPosition(vec3 position, vec3 direction) {
+        this.driver.setPosition(new Pose2D(
+                DistanceUnit.METER,
+                position.x,
+                position.y,
+                AngleUnit.RADIANS,
+                Math.atan2(direction.y, direction.x)
+        ));
+    }
 
     @Override
     public void update(double delta) {
         this.driver.update();
 
         Pose2D rawPose = this.driver.getPosition();
+        double heading = rawPose.getHeading(AngleUnit.RADIANS);
 
         this.position.x = rawPose.getX(DistanceUnit.METER);
         this.position.y = rawPose.getY(DistanceUnit.METER);
-        this.direction.x = Math.cos(rawPose.getHeading(AngleUnit.RADIANS));
-        this.direction.y = Math.sin(rawPose.getHeading(AngleUnit.RADIANS));
+        this.direction.x = Math.cos(heading);
+        this.direction.y = Math.sin(heading);
+
+        vec2 velocity = new vec2(this.driver.getVelX(DistanceUnit.METER), this.driver.getVelY(DistanceUnit.METER));
+        velocity.rotate(-heading);
+
+        this.velocity.x = velocity.x;
+        this.velocity.y = velocity.y;
+        this.angularVel.z = this.driver.getHeadingVelocity(UnnormalizedAngleUnit.RADIANS);
     }
 
     @Override
