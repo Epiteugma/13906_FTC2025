06c21ddbba4e1a65415ce88dc3f83144192b46f0
z3db0y <z3db0y@users.noreply.github.com>
2026-02-07 17:50:07 +0200

    slightly change shooter coeffs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turret.java
@@ -98,8 +98,8 @@ public class Turret {
         static final double RATIO_B = 10 / 15.0;
         static final double RPM = 6000;
 
-        static final double EFFICIENCY = 0.37;
-        static final double ARTIFACT_LOAD_VELOCITY = 0.85;
+        static final double EFFICIENCY = 0.35;
+        static final double ARTIFACT_LOAD_VELOCITY = 1.1;
 
         static final double FLYWHEEL_RADIUS = 0.045;
         static final double MAX_FLYWHEEL_VELOCITY = RPM * RATIO / 60.0 * (2 * Math.PI);
@@ -158,9 +158,9 @@ public class Turret {
         if (alliance == Robot.Alliance.UNKNOWN) return null;
 
         return new Basket(alliance == Robot.Alliance.RED ? RED_BASKET : BLUE_BASKET, new vec3(
-                robotPosition.x + robotVelocity.x * 0.2,
-                robotPosition.y + robotVelocity.y * 0.2,
-                robotPosition.z + robotVelocity.z * 0.2
+                robotPosition.x, // + robotVelocity.x * 0.2,
+                robotPosition.y, // + robotVelocity.y * 0.2,
+                robotPosition.z  // + robotVelocity.z * 0.2
         ));
     }
 
